10590017a198739e25ccea41fd6549ccbaef3fe0
Buddha Lo <lo_977@yahoo.com>
2024-05-01 23:55:06 -0700

    complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [isFilter, setIsFilter] = useState("All");
+  const renderList =
+    isFilter === "All"
+      ? items
+      : items.filter((item) => item.category === isFilter);
+  console.log(renderList);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          value={isFilter}
+          onChange={(e) => setIsFilter(e.target.value)}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {renderList.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
